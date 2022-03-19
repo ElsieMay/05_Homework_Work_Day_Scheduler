94b4a2e45838132362900c3087ef5b386dce321a
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-19 14:11:21 +1100

    added table with timeblocks and colour

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+var currentHour = new Date().getHours();
+$(".colorcode").each(function () {
+	var val = parseInt($(this).prop("id"));
+	if (val > currentHour && val < currentHour + 6) {
+		$(this).css("background-color", "Blue");
+	} else if (val < currentHour && val > currentHour - 6) {
+		$(this).css("background-color", "Red");
+	} else if (val === currentHour) {
+		$(this).css("background-color", "Green");
+	} else {
+		$(this).css("background-color", "White");
+	}
+});
