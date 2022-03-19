44a7a6ac41234fc2d178b5dd200f66609dbe69ff
ElsieMay <elsiemaylawrie1@gmail.com>
2022-03-19 16:41:51 +1100

    fixed time colour coding

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,22 @@
-var present = document.querySelector(".present");
-var past = document.querySelector(".past");
-var future = document.querySelector(".future");
 //Prints current date//
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
-//Colour codes rows based on time tense//
-var currentHour = Date.now().getHours();
+
+//Colour codes time//
+var currentHour = new Date().getHours();
 $(".colorcode").each(function () {
 	var val = parseInt($(this).prop("id"));
 	if (val > currentHour && val < currentHour + 6) {
-		$(this).css(future);
+		$(this).css("background-color", "Blue");
 	} else if (val < currentHour && val > currentHour - 6) {
-		$(this).css(past);
+		$(this).css("background-color", "Red");
 	} else if (val === currentHour) {
-		$(this).css(present);
+		$(this).css("background-color", "Green");
+	} else {
+		$(this).css("background-color", "White");
 	}
 });
+
 //Saves event on click//
 $(document).ready(function () {
 	// Get value on button click
